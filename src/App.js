f19fa4bba06d149057d46933900759de1a54a40d
Ganesh Kanna MC <emcydeveloper@gmail.com>
2022-04-26 00:20:38 +0530

    UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./style.css"
 
 function App() {
+  const [post, setPost] = useState([]);
+
+  let fetchPost = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "get",
+    })
+      .then((postData) => postData.json())
+      .then((postList) => setPost(postList))
+      .then(() => console.log("Home page loaded with Post"));
+  };
+
+  useEffect(() => {
+    fetchPost();
+  }, []);
+
+  // post.map((a) => console.log(a));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>welcome</h1> */}
+      <DisplayPost post={post} setPost={setPost} />
     </div>
   );
 }
 
 export default App;
+
+function DisplayPost({ post, setPost }) {
+  return (
+    <div className="display-post">
+      <button>Add new post</button>
+      <div className="item-container">
+      {post.map((postItem, i) => (
+        <div key={i} className="display-post-items">
+        {/* <h2>User Id: {postItem.userId}</h2> */}
+        {/* <h2>ID: {postItem.id}</h2> */}
+        <div className="title"><h2>Title: {postItem.title}</h2></div>
+        {/* <h3>Body</h3> */}
+        <div className="body">{postItem.body}</div>
+        <div className="item-action"><button>Edit</button><button>delete</button></div>
+        
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+}
