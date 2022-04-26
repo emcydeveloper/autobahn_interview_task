a65586763a6d3d97c64f22343b7c245c49d347fe
Ganesh Kanna MC <emcydeveloper@gmail.com>
2022-04-26 18:03:40 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { useEffect, useState } from "react";
-import "./style.css"
+import { useParams } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+import "./style.css";
 
 function App() {
   const [post, setPost] = useState([]);
@@ -17,12 +23,20 @@ function App() {
     fetchPost();
   }, []);
 
-  // post.map((a) => console.log(a));
-
   return (
     <div className="App">
-      {/* <h1>welcome</h1> */}
-      <DisplayPost post={post} setPost={setPost} />
+      
+      <Switch>
+        <Route path="/edit/:id">
+          <EditPost post={post} setPost={setPost} />
+        </Route>
+        <Route path="/add">
+        <CreatePost post={post} setPost={setPost} />
+        </Route>
+        <Route path="/">
+          <DisplayPost post={post} setPost={setPost} />
+        </Route>
+      </Switch>
     </div>
   );
 }
@@ -30,21 +44,166 @@ function App() {
 export default App;
 
 function DisplayPost({ post, setPost }) {
+  const history = useHistory();
+  const handleEdit = (id) => {
+    history.push("/edit/" + id);
+  };
+  const handleDelete = (id)=>{
+    let newArray = post.filter(item=>parseInt(item.id)!==parseInt(id))
+    setPost(newArray)
+  }
+  
   return (
     <div className="display-post">
       <button>Add new post</button>
       <div className="item-container">
-      {post.map((postItem, i) => (
-        <div key={i} className="display-post-items">
-        {/* <h2>User Id: {postItem.userId}</h2> */}
-        {/* <h2>ID: {postItem.id}</h2> */}
-        <div className="title"><h2>Title: {postItem.title}</h2></div>
-        {/* <h3>Body</h3> */}
-        <div className="body">{postItem.body}</div>
-        <div className="item-action"><button>Edit</button><button>delete</button></div>
-        
-        </div>
-      ))}
+        {post.map((postItem, i) => (
+          <div key={i} className="display-post-items">
+            <div className="title">
+              <h2>
+                {postItem.id}: {postItem.title}
+              </h2>
+            </div>
+            <div className="body">{postItem.body}</div>
+            <div className="item-action">
+              <button onClick={() => handleEdit(postItem.id)}>Edit</button>
+              <button onClick={() => handleDelete(postItem.id)}>delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function EditPost({ post, setPost }) {
+  let [title, setTitle] = useState("");
+  let [body, setBody] = useState("");
+
+  let history = useHistory();
+  let { id } = useParams();
+  let getEditItem = () =>
+    post.filter((item) => {
+      if (parseInt(item.id) === parseInt(id)) {
+        setTitle(item.title);
+        setBody(item.body);
+      }
+    });
+  useEffect(getEditItem, []);
+  let handleCancel = () => {
+    history.push("/home");
+  };
+
+  const onSubmit = (event) => {
+    post.filter((item) => {
+      if (parseInt(item.id) === parseInt(id)) {
+        item.title = title;
+        item.body = body;
+        history.push("/home");
+      }
+
+      return item;
+    });
+  };
+
+  let handleTitle = (event) => setTitle(event.target.value);
+  let handleBody = (event) => setBody(event.target.value);
+
+  return (
+    <div>
+      <div>Edit Post- {id}</div>
+      <div className="edit">
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": {
+              m: 1,
+              width: "40ch",
+              display: "flex",
+              flexWrap: "wrap",
+            },
+          }}
+          noValidate
+          autoComplete="off"
+          onSubmit={onSubmit}
+        >
+          <TextField
+            id="title"
+            onChange={handleTitle}
+            value={title}
+            name="title"
+          />
+          <TextField id="body" onChange={handleBody} value={body} name="Body" />
+
+          <div className="btn-container">
+            <button className="success">Save</button>
+            <button onClick={handleCancel} className="cancel">
+              cancel
+            </button>
+          </div>
+        </Box>
+      </div>
+    </div>
+  );
+}
+
+
+
+function CreatePost({ post, setPost }) {
+  let [title, setTitle] = useState("");
+  let [body, setBody] = useState("");
+  let [data,setData]=useState({});
+
+  let history = useHistory();
+
+  let handleCancel = () => {
+    history.push("/home");
+  };
+
+  const onSubmit = (event) => {
+    
+    setData({userId:"",id:"",title:title,body:body})
+    
+    setPost([...post,data])
+    history.push("/home");
+    event.preventDefault();
+  };
+
+  let handleTitle = (event) => setTitle(title = event.target.value);
+  let handleBody = (event) => setBody(event.target.value);
+
+  return (
+    <div>
+      <div className="edit">
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": {
+              m: 1,
+              width: "40ch",
+              display: "flex",
+              flexWrap: "wrap",
+            },
+          }}
+          noValidate
+          autoComplete="off"
+          onSubmit={onSubmit}
+        >
+          <TextField
+            id="title"
+            onChange={handleTitle}
+            value={title}
+            name="title"
+          />
+          <TextField id="body" onChange={handleBody} value={body} name="Body" />
+
+          <div className="btn-container">
+            <button className="success">Save</button>
+            <button onClick={handleCancel} className="cancel">
+              cancel
+            </button>
+          </div>
+        </Box>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router} from 'react-router-dom';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
+  <Router>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </Router>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
